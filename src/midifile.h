0ecd55bffc5dfe06510442b1ebec6430f9917b75
Bert Mueller <bm3_2000@yahoo.com>
2021-10-16 17:25:31 +0100

    v4
    - includes latest updates from Div Slomin (20210908)

--- src/midifile.h
+++ src/midifile.h
@@ -3,7 +3,7 @@
 
 /*
  * Div's Standard MIDI File API
- * Copyright 2003-2020 by David G. Slomin
+ * Copyright 2003-2021 by David G. Slomin
  * Provided under the terms of the BSD license
  *
  * Usage notes:
@@ -17,47 +17,55 @@
  *     first track as a "conductor track" for meta events like tempo and
  *     meter changes.
  *
- * 3.  MidiFile_visitEvents() and MidiFileTrack_visitEvents() are specially
- *     designed so that you can add, delete, or change the tick of events
- *     (thereby modifying the sorting order) without upsetting the iterator.
- *
- * 4.  Any data passed into these functions is memory-managed by the caller.
+ * 3.  Any data passed into these functions is memory-managed by the caller.
  *     Any data returned from these functions is memory-managed by the API.
  *     Don't forget to call MidiFile_free().
  *
- * 5.  This API is not thread-safe.
+ * 4.  This API is not thread-safe.
  *
- * 6.  You can navigate through events one track at a time, or through all
- *     tracks at once in an interwoven, time-sorted manner.
- *
- * 7.  Because a note on event with a velocity of zero is functionally
- *     equivalent to a note off event, you cannot simply look at the type of
- *     an event to see whether it signifies the start or the end of a note.
- *     To handle this problem, convenience wrappers are provided for pseudo
- *     "note start" and "note end" events.
- *
- * 8.  Convenience functions are provided for working with tempo, absolute
- *     time, musical time, and SMPTE time in files of format 1 or 0.
- *
- * 9.  Events other than sysex and meta (i.e., events which have a channel)
- *     are considered "voice events".  For interaction with other APIs, it
- *     is sometimes useful to pack their messages into 32 bit integers.
- *
- * 10. Key signatures are expressed as a negative number of flats or a
- *     positive number of sharps.
- *
- * 11. All numbers in this API are zero-based, to correspond with the actual
+ * 5.  All numbers in this API are zero-based, to correspond with the actual
  *     byte values of the MIDI protocol, rather than one-based, as they are
  *     commonly displayed to the user.  Channels range from 0 to 15, notes
  *     range from 0 to 127, etc.  The only exception is in the musical time
  *     functions.
  *
- * 12. An event can be "detached", which means that it is no longer part of
+ * 6.  You can navigate through events one track at a time, or through all
+ *     tracks at once in an interwoven, time-sorted manner.
+ *
+ * 7.  MidiFile_iterateEvents() and MidiFileTrack_iterateEvents() are specially
+ *     designed so that you can add, delete, or change the tick of the current
+ *     event (thereby modifying the sorting order) without upsetting the
+ *     iterator.  Keep calling them until they return null.
+ *
+ * 8.  Because a note on event with a velocity of zero is functionally
+ *     equivalent to a note off event, you cannot simply look at the type of
+ *     an event to see whether it signifies the start or the end of a note.
+ *     To handle this problem, convenience wrappers are provided for pseudo
+ *     "note start" and "note end" events.
+ *
+ * 9.  Alternatively, it's often useful to think of a note as a single event
+ *     with a duration rather than separate events for the start and end.
+ *     Use MidiFile_convertStandardEventsToNoteEvents() to switch to this
+ *     representation.  Similar conversions are provided for fine (14 bit)
+ *     control change events, RPN events, and NRPN events.  Don't forget
+ *     to convert back to standard events before saving.
+ *
+ * 10. Convenience functions are provided for working with tempo, absolute
+ *     time, musical time, and SMPTE time in files of format 1 or 0.
+ *
+ * 11. Events other than sysex and meta (i.e., events which have a channel)
+ *     are considered "voice events".  For interaction with other APIs, it
+ *     is sometimes useful to pack their messages into 32 bit integers.
+ *
+ * 12. Key signatures are expressed as a negative number of flats or a
+ *     positive number of sharps.
+ *
+ * 13. An event can be "detached", which means that it is no longer part of
  *     a track or the file, but retains the rest of its properties.  It can
  *     be reattached to the same or a different file by setting its track
  *     property, or explicitly deleted.
  *
- * 13. Events can be marked as "selected" but this is only meaningful in
+ * 14. Events can be marked as "selected" but this is only meaningful in
  *     memory; it is not persisted to disk.
  */
 
@@ -97,7 +105,11 @@ typedef enum
 	MIDI_FILE_EVENT_TYPE_CHANNEL_PRESSURE,
 	MIDI_FILE_EVENT_TYPE_PITCH_WHEEL,
 	MIDI_FILE_EVENT_TYPE_SYSEX,
-	MIDI_FILE_EVENT_TYPE_META
+	MIDI_FILE_EVENT_TYPE_META,
+	MIDI_FILE_EVENT_TYPE_NOTE,
+	MIDI_FILE_EVENT_TYPE_FINE_CONTROL_CHANGE,
+	MIDI_FILE_EVENT_TYPE_RPN,
+	MIDI_FILE_EVENT_TYPE_NRPN
 }
 MidiFileEventType_t;
 
@@ -108,6 +120,7 @@ int MidiFile_saveToBuffer(MidiFile_t midi_file, unsigned char *buffer);
 int MidiFile_getFileSize(MidiFile_t midi_file);
 
 MidiFile_t MidiFile_new(int file_format, MidiFileDivisionType_t division_type, int resolution);
+MidiFile_t MidiFile_newFromTemplate(MidiFile_t template_midi_file);
 int MidiFile_free(MidiFile_t midi_file);
 int MidiFile_getFileFormat(MidiFile_t midi_file);
 int MidiFile_setFileFormat(MidiFile_t midi_file, int file_format);
@@ -124,9 +137,16 @@ MidiFileTrack_t MidiFile_getFirstTrack(MidiFile_t midi_file);
 MidiFileTrack_t MidiFile_getLastTrack(MidiFile_t midi_file);
 MidiFileEvent_t MidiFile_getFirstEvent(MidiFile_t midi_file);
 MidiFileEvent_t MidiFile_getLastEvent(MidiFile_t midi_file);
+MidiFileEvent_t MidiFile_iterateEvents(MidiFile_t midi_file);
 int MidiFile_visitEvents(MidiFile_t midi_file, MidiFileEventVisitorCallback_t visitor_callback, void *user_data);
 int MidiFile_convertSelectionFlagsToTextEvents(MidiFile_t midi_file, char *label);
 int MidiFile_convertTextEventsToSelectionFlags(MidiFile_t midi_file, char *label);
+int MidiFile_convertStandardEventsToNoteEvents(MidiFile_t midi_file);
+int MidiFile_convertNoteEventsToStandardEvents(MidiFile_t midi_file);
+int MidiFile_convertStandardEventsToFineControlChangeEvents(MidiFile_t midi_file);
+int MidiFile_convertFineControlChangeEventsToStandardEvents(MidiFile_t midi_file);
+int MidiFile_convertStandardEventsToRpnAndNrpnEvents(MidiFile_t midi_file);
+int MidiFile_convertRpnAndNrpnEventsToStandardEvents(MidiFile_t midi_file);
 
 float MidiFile_getBeatFromTick(MidiFile_t midi_file, long tick);
 long MidiFile_getTickFromBeat(MidiFile_t midi_file, float beat);
@@ -178,6 +198,10 @@ MidiFileEvent_t MidiFileTrack_createChannelPressureEvent(MidiFileTrack_t track, 
 MidiFileEvent_t MidiFileTrack_createPitchWheelEvent(MidiFileTrack_t track, long tick, int channel, int value);
 MidiFileEvent_t MidiFileTrack_createSysexEvent(MidiFileTrack_t track, long tick, int data_length, unsigned char *data_buffer);
 MidiFileEvent_t MidiFileTrack_createMetaEvent(MidiFileTrack_t track, long tick, int number, int data_length, unsigned char *data_buffer);
+MidiFileEvent_t MidiFileTrack_createNoteEvent(MidiFileTrack_t track, long tick, long duration_ticks, int channel, int note, int start_velocity, int end_velocity);
+MidiFileEvent_t MidiFileTrack_createFineControlChangeEvent(MidiFileTrack_t track, long tick, int channel, int coarse_number, int value);
+MidiFileEvent_t MidiFileTrack_createRpnEvent(MidiFileTrack_t track, long tick, int channel, int number, int value);
+MidiFileEvent_t MidiFileTrack_createNrpnEvent(MidiFileTrack_t track, long tick, int channel, int number, int value);
 MidiFileEvent_t MidiFileTrack_createNoteStartAndEndEvents(MidiFileTrack_t track, long start_tick, long end_tick, int channel, int note, int start_velocity, int end_velocity); /* returns the start event */
 MidiFileEvent_t MidiFileTrack_createTextEvent(MidiFileTrack_t track, long tick, char *text);
 MidiFileEvent_t MidiFileTrack_createLyricEvent(MidiFileTrack_t track, long tick, char *text);
@@ -187,8 +211,10 @@ MidiFileEvent_t MidiFileTrack_createTempoEvent(MidiFileTrack_t track, long tick,
 MidiFileEvent_t MidiFileTrack_createTimeSignatureEvent(MidiFileTrack_t track, long tick, int numerator, int denominator);
 MidiFileEvent_t MidiFileTrack_createKeySignatureEvent(MidiFileTrack_t track, long tick, int number, int minor);
 MidiFileEvent_t MidiFileTrack_createVoiceEvent(MidiFileTrack_t track, long tick, unsigned long data);
+MidiFileEvent_t MidiFileTrack_copyEvent(MidiFileTrack_t track, MidiFileEvent_t event);
 MidiFileEvent_t MidiFileTrack_getFirstEvent(MidiFileTrack_t track);
 MidiFileEvent_t MidiFileTrack_getLastEvent(MidiFileTrack_t track);
+MidiFileEvent_t MidiFileTrack_iterateEvents(MidiFileTrack_t track);
 int MidiFileTrack_visitEvents(MidiFileTrack_t track, MidiFileEventVisitorCallback_t visitor_callback, void *user_data);
 
 int MidiFileEvent_delete(MidiFileEvent_t event);
@@ -208,7 +234,6 @@ int MidiFileEvent_setTick(MidiFileEvent_t event, long tick);
 MidiFileEventType_t MidiFileEvent_getType(MidiFileEvent_t event);
 int MidiFileEvent_isSelected(MidiFileEvent_t event);
 int MidiFileEvent_setSelected(MidiFileEvent_t event, int is_selected);
-int MidiFileEvent_isNoteEvent(MidiFileEvent_t event);
 int MidiFileEvent_isNoteStartEvent(MidiFileEvent_t event);
 int MidiFileEvent_isNoteEndEvent(MidiFileEvent_t event);
 int MidiFileEvent_isPressureEvent(MidiFileEvent_t event);
@@ -273,6 +298,60 @@ int MidiFileMetaEvent_setNumber(MidiFileEvent_t event, int number);
 int MidiFileMetaEvent_getDataLength(MidiFileEvent_t event);
 unsigned char *MidiFileMetaEvent_getData(MidiFileEvent_t event);
 int MidiFileMetaEvent_setData(MidiFileEvent_t event, int data_length, unsigned char *data_buffer);
+
+long MidiFileNoteEvent_getDurationTicks(MidiFileEvent_t event);
+int MidiFileNoteEvent_setDurationTicks(MidiFileEvent_t event, long duration_ticks);
+int MidiFileNoteEvent_getChannel(MidiFileEvent_t event);
+int MidiFileNoteEvent_setChannel(MidiFileEvent_t event, int channel);
+int MidiFileNoteEvent_getNote(MidiFileEvent_t event);
+int MidiFileNoteEvent_setNote(MidiFileEvent_t event, int note);
+int MidiFileNoteEvent_getVelocity(MidiFileEvent_t event);
+int MidiFileNoteEvent_setVelocity(MidiFileEvent_t event, int velocity);
+int MidiFileNoteEvent_getEndVelocity(MidiFileEvent_t event);
+int MidiFileNoteEvent_setEndVelocity(MidiFileEvent_t event, int end_velocity);
+
+int MidiFileFineControlChangeEvent_getChannel(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setChannel(MidiFileEvent_t event, int channel);
+int MidiFileFineControlChangeEvent_getCoarseNumber(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setCoarseNumber(MidiFileEvent_t event, int coarse_number);
+int MidiFileFineControlChangeEvent_getFineNumber(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setFineNumber(MidiFileEvent_t event, int fine_number);
+int MidiFileFineControlChangeEvent_getValue(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setValue(MidiFileEvent_t event, int value);
+int MidiFileFineControlChangeEvent_getCoarseValue(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setCoarseValue(MidiFileEvent_t event, int coarse_value);
+int MidiFileFineControlChangeEvent_getFineValue(MidiFileEvent_t event);
+int MidiFileFineControlChangeEvent_setFineValue(MidiFileEvent_t event, int fine_value);
+
+int MidiFileRpnEvent_getChannel(MidiFileEvent_t event);
+int MidiFileRpnEvent_setChannel(MidiFileEvent_t event, int channel);
+int MidiFileRpnEvent_getNumber(MidiFileEvent_t event);
+int MidiFileRpnEvent_setNumber(MidiFileEvent_t event, int number);
+int MidiFileRpnEvent_getCoarseNumber(MidiFileEvent_t event);
+int MidiFileRpnEvent_setCoarseNumber(MidiFileEvent_t event, int coarse_number);
+int MidiFileRpnEvent_getFineNumber(MidiFileEvent_t event);
+int MidiFileRpnEvent_setFineNumber(MidiFileEvent_t event, int fine_number);
+int MidiFileRpnEvent_getValue(MidiFileEvent_t event);
+int MidiFileRpnEvent_setValue(MidiFileEvent_t event, int value);
+int MidiFileRpnEvent_getCoarseValue(MidiFileEvent_t event);
+int MidiFileRpnEvent_setCoarseValue(MidiFileEvent_t event, int coarse_value);
+int MidiFileRpnEvent_getFineValue(MidiFileEvent_t event);
+int MidiFileRpnEvent_setFineValue(MidiFileEvent_t event, int fine_value);
+
+int MidiFileNrpnEvent_getChannel(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setChannel(MidiFileEvent_t event, int channel);
+int MidiFileNrpnEvent_getNumber(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setNumber(MidiFileEvent_t event, int number);
+int MidiFileNrpnEvent_getCoarseNumber(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setCoarseNumber(MidiFileEvent_t event, int coarse_number);
+int MidiFileNrpnEvent_getFineNumber(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setFineNumber(MidiFileEvent_t event, int fine_number);
+int MidiFileNrpnEvent_getValue(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setValue(MidiFileEvent_t event, int value);
+int MidiFileNrpnEvent_getCoarseValue(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setCoarseValue(MidiFileEvent_t event, int coarse_value);
+int MidiFileNrpnEvent_getFineValue(MidiFileEvent_t event);
+int MidiFileNrpnEvent_setFineValue(MidiFileEvent_t event, int fine_value);
 
 int MidiFileNoteStartEvent_getChannel(MidiFileEvent_t event);
 int MidiFileNoteStartEvent_setChannel(MidiFileEvent_t event, int channel);
